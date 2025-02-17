c3fb5d457c1b63d3c8d8501cda06afb47a204f26
klaudiarogoda <klaudiarogoda@gmail.com>
2025-02-17 20:24:37 +0100

    Daily iMessage Sender

---
+++ src/MessageScheduler.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MessageScheduler {
+    private MessageService messageService;
+    private String [] messages;
+    int daysSent;
+
+
+    public MessageScheduler(MessageService messageService, String[] messages){
+        this.messageService=messageService;
+        this.messages=messages;
+    }
+
+    public void scheduleMessage(){
+        ScheduledExecutorService scheduler= Executors.newSingleThreadScheduledExecutor();
+        long delay=calculateDelay(8);
+        int randomNumber=(int) (Math.random() * messages.length);
+        String todaysMessage=messages[randomNumber];
+        System.out.println("Today's message: "+todaysMessage);
+        scheduler.scheduleAtFixedRate(()->{
+            if(daysSent>=3) {
+                stopScheduler(scheduler);
+                return;
+            }
+            messageService.sendMessage(todaysMessage);
+            daysSent++;
+        },delay, TimeUnit.DAYS.toMillis(1),TimeUnit.MILLISECONDS);
+    }
+    public long calculateDelay(int hour){
+        Calendar currentTime=Calendar.getInstance();
+
+        Calendar targetTime = Calendar.getInstance();
+        targetTime.set(Calendar.HOUR_OF_DAY, hour);
+        targetTime.set(Calendar.MINUTE, 0);
+        targetTime.set(Calendar.SECOND, 0);
+        targetTime.set(Calendar.MILLISECOND, 0);
+
+        if(currentTime.after(targetTime)){
+            targetTime.add(Calendar.DATE, 1);
+        }
+        long delay=targetTime.getTimeInMillis()-currentTime.getTimeInMillis();
+        return delay;
+    }
+
+    public void stopScheduler(ScheduledExecutorService scheduler){
+        scheduler.shutdown();
+        try {
+            if (!scheduler.awaitTermination(30, TimeUnit.SECONDS)) {
+                scheduler.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            scheduler.shutdownNow();
+        }
+
+    }
+}
---
+++ src/MessageService.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.*;
+public class MessageService {
+    public void sendMessage(String message){
+        try{
+            //AppleScript
+            String script = "tell application \"Messages\"\n" +
+                    "set targetBuddy to \"+48450000543\"\n" +
+                    "send \"" + message + "\" to buddy targetBuddy\n" +
+                    "end tell";
+
+
+            Process process = Runtime.getRuntime().exec(new String[]{"/usr/bin/osascript", "-e", script});
+            process.waitFor();
+            System.out.println("Message sent");
+
+        }catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
